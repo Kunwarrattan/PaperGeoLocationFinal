7ac2aaa1bb867ae4f0bc709939c15fd472d11f73
Kunwar Rattan <kunwarrattan@live.com>
2015-04-23 23:43:57 -0400

    Filter in one file

--- database/Filter.php
+++ database/Filter.php
@@ -10,12 +10,12 @@
 require 'database.php';
 $rowid = null;
 $k=1;
-echo "asdasd";
-for($index=36000;$index<50000;$index=$index+50){
 
-    $query2 = 'SELECT * FROM `address_unique` where `latlongid` is NULL Limit '.($index*1).", 50 ";
+//for($index=36000;$index<50000;$index=$index+50){
+$index = 2;
+    $query2 = 'SELECT * FROM `address_unique` where `latlongid` is NULL;# Limit '.($index*1).", 50 ";
     $result2 = mysqli_query($link, $query2);
-    //echo $result2->num_rows;
+    echo $result2->num_rows."<br/>";
     if($result2->num_rows>0){
     while ($row = mysqli_fetch_assoc($result2)) {
         $idN  = $row['institute'];
@@ -24,22 +24,42 @@ for($index=36000;$index<50000;$index=$index+50){
             $ville = $row['ville'];
             $province = $row['province'];
             $Country = $row['Country'];
-                //echo "<br/>".$inst."------<br/>";
+               // echo "<br/>".$inst."------<br/>";
                 $query1 = "SELECT * FROM `address_unique` where `latlongid` is NOT NULL";
                 $result1 = mysqli_query($link, $query1);
                // echo $result1->num_rows;
                 if($result1->num_rows>0) {
                     while ($row = mysqli_fetch_assoc($result1)) {
                         if ($ville == $row['ville'] && $province == $row['province'] && $Country == $row['Country'] && $id != $row['id'] && $inst != null && $row['institute'] != null) {
+
+                            $length1 = strlen($inst);
+                            $length2 = strlen($row['institute']);
+                            $str1 = strtolower(substr($inst,0,$length1/2));
+                            $str2 = strtolower(substr($row['institute'],0,$length2/2));
+                            echo $str1 ." = " . $row['institute']." ====== ".$str2 . " = " . $inst . "<br/>";
+
                                 if (stristr($inst, $row['institute']) || stristr($row['institute'], $inst)) {
-                                    echo $row['id'] . " <-------".$k."------------>  " . $row['institute'] . "     <-------------------->  " . $inst;
+                                    echo $row['id'] . " <- Level 1 match------".$k."------------>  " . $row['institute'] . "     <-------------------->  " . $inst;
                                     echo "<br />";
-                                    $newID = $row['id'];
+                                    $newID = $row['latlongid'];
                                     $queryUpdate ="\n UPDATE `address_unique` set `latlongid` = $newID WHERE `id` = $id";
                                     $result9 = mysqli_query($link, $queryUpdate);
                                     echo $queryUpdate;
                                     echo "<br />";
                                     $k++;
+                                    break;
+                                }elseif(stristr($str1, $str2) || stristr($str2, $str1)){
+                                    echo $row['id'] . " <- Level 2 Match ------".$k."------------>  " . $str1 . "     <-------------------->  " . $str2;
+                                    echo "<br />";
+                                    $newID = $row['latlongid'];
+                                    $queryUpdate ="\n UPDATE `address_unique` set `latlongid` = $newID WHERE `id` = $id";
+                                    $result9 = mysqli_query($link, $queryUpdate);
+                                    echo $queryUpdate;
+                                    echo "<br />";
+                                    $k++;
+                                    break;
+                                }else{
+                                    echo "I am no where<br />";
                                     break;
                                 }
                         }
@@ -50,5 +70,5 @@ for($index=36000;$index<50000;$index=$index+50){
     if (!$query2) {
         echo 'MySQL Error: ' . mysqli_error($link);
         exit;
-    }
+   # }
 }
--- database/FilterTable.php
+++ database/FilterTable.php
@@ -10,11 +10,11 @@
 require 'database.php';
 $rowid = null;
 $k=1;
-for($index=1;$index<10000;$index=$index+50){
-
-    $query2 = 'SELECT * FROM `address_unique` where `latlongid` is NULL Limit '.($index*1).", 50 ";
+#for($index=1;$index<10000;$index=$index+50){
+$index = 2;
+    $query2 = 'SELECT * FROM `address_unique` where `latlongid` is NULL';#'  Limit '.($index*1).", 50 ";
     $result2 = mysqli_query($link, $query2);
-    //echo $result2->num_rows;
+    echo $result2->num_rows;
     if($result2->num_rows>0){
     while ($row = mysqli_fetch_assoc($result2)) {
         $idN  = $row['institute'];
@@ -23,17 +23,29 @@ for($index=1;$index<10000;$index=$index+50){
             $ville = $row['ville'];
             $province = $row['province'];
             $Country = $row['Country'];
-                //echo "<br/>".$inst."------<br/>";
+
+                echo "<br/>".$inst."<br/>";
                 $query1 = "SELECT * FROM `address_unique` where `latlongid` is NOT NULL";
                 $result1 = mysqli_query($link, $query1);
-               // echo $result1->num_rows;
+                echo $result1->num_rows."<br/>";
                 if($result1->num_rows>0) {
                     while ($row = mysqli_fetch_assoc($result1)) {
                         if ($ville == $row['ville'] && $province == $row['province'] && $Country == $row['Country'] && $id != $row['id'] && $inst != null && $row['institute'] != null) {
-                                if (stristr($inst, $row['institute']) || stristr($row['institute'], $inst)) {
-                                    echo $row['id'] . " <-------".$k."------------>  " . $row['institute'] . "     <-------------------->  " . $inst;
-                                    echo "<br />";
-                                    $newID = $row['id'];
+                            $latlong = $row['latlongid'];
+                            $length1 = strlen($inst);
+                            $length2 = strlen($row['institute']);
+
+                            $str1 = substr($inst,0,$length1/2);
+                            $str2 = substr($row['institute'],0,$length2/2);
+
+                            echo $str1." == ". $row['institute'];
+                            echo "<br/>".$str2." == ". $inst;
+                           // echo $row['id'] . " <-------".$k."------------>  " . $str1 . "     <-------------------->  " . $str2;
+                            if($str1 === $str2){
+                                #if (stristr($str1, $row['institute']) || stristr($str2, $inst)) {
+                                   // echo $row['id'] . " <-------".$k."------------>  " . $str1 . "     <-------------------->  " . $str2;
+                                   // echo "<br />";
+                                    $newID = $row['latlongid'];
                                     $queryUpdate ="\n UPDATE `address_unique` set `latlongid` = $newID WHERE `id` = $id";
                                     $result9 = mysqli_query($link, $queryUpdate);
                                     echo $queryUpdate;
@@ -45,7 +57,7 @@ for($index=1;$index<10000;$index=$index+50){
                     }
                 }
         }
-    }
+    #}
     if (!$query2) {
         echo 'MySQL Error: ' . mysqli_error($link);
         exit;
