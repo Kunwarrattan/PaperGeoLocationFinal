03d64d5497f0cff86bc90e9bdb84a387c5b17ec5
Kunwar Rattan <kunwarrattan@live.com>
2015-05-25 18:28:03 -0400

    commit
    asdasd

--- database/testAddressToadreessunique.php
+++ database/testAddressToadreessunique.php
@@ -6,64 +6,68 @@
  * Time: 6:31 PM
  */
 
+//this file is mapping data to the addresses form address unique
 
 header("Content-Type: text/html;charset=utf-8");
 set_time_limit(0);
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
 
-require 'database.php';
+if (!$link = mysqli_connect('localhost', 'root', '')) {
+    echo 'Could not connect to mysql';
+    exit;
+}
+
+if (!mysqli_select_db($link,'geotest')) {
+    echo 'Could not select database';
+    exit;
+}
 
 //for($index=1;$index<1000;$index=$index+100){
 
-$index = 1;
-
-    $query1 = ' SELECT * FROM `addresses` where mapID is NULL  Limit '.($index*1).",10 ";
+for($index =1 ;$index <100 ;$index= $index+50)
+{
+    $query1 = ' SELECT * FROM `addresses` where `Country` != "" and  `mapID` is NULL  Limit '.($index*1).",50 ";
     $result1 = mysqli_query($link, $query1);
 
     if($result1->num_rows>0){
         while ($row = mysqli_fetch_assoc($result1)) {
             $id1 = $row['id_art'];
             $order = $row['ordre'];
-            $inst  = $row['institution'];
+            $inst = $row['institution'];
             $ville = $row['ville'];
             $province = $row['province'];
             $country = $row['Country'];
+            $latlongID = NULL;
 
-            //echo $row['id_art']." ".$row['ordre']."<br />";
-
-            $query2 = "INSERT INTO `address_unique_test` (`institute`, `ville`, `province`, `Country`) VALUES ".
-                            "(\"$inst\",\"$ville\",\"$province\",\"$country\")";
-
-            echo $query2."<br/>";
-
+            $query = "INSERT INTO `address_unique`( `institute`, `ville`, `province`,`Country`) VALUES (\"$inst\",\"$ville\",\"$province\",\"$country\")";
+            //mysqli_query("utf8",$query);
+            echo "<br/>".$query;
             mysqli_set_charset($link, "utf8");
             mysqli_query($link, "SET NAMES 'utf8'");
             mysqli_query($link, "SET CHARACTER SET 'utf8'");
-            $result2 = mysqli_query($link, $query2);
+            $result = mysqli_query($link, $query);
 
             $idN = mysqli_insert_id($link);
+            echo "->".$idN;
 
-           if($idN == null){
-                $query3 = "SELECT `id` FROM `address_unique_test` where `institute` = \"$inst\" and `ville` = \"$ville\"  and `province` = \"$province\" and `Country` = \"$country\"";
-                echo $query3."<br/>";
-                $result2 = mysqli_query($link, $query3);
+            $query2 = null;
+            if($idN == null){
+                $query2 = "SELECT `id`,`latlongid` FROM `address_unique` where `institute`= \"$inst\" AND `ville` = \"$ville\" AND  `province` = \"$province\" AND `Country` = \"$country\" ";
+                $result2 = mysqli_query($link, $query2);
+                echo "<br/>".$query2;
                 while ($row = mysqli_fetch_assoc($result2)) {
                     $idN  = $row['id'];
+                    $latlongID = $row['latlongid'];
+
                 }
             }
-//
-//
-            $queryUpdate ="UPDATE `addresses` set `mapID` = $idN WHERE `id_art` = $id1 and `ordre` = $order";
-            $result5 = mysqli_query($link, $queryUpdate);
-            echo $queryUpdate."<br />";
+            echo "->".$idN;
+            $queryUpdate ="UPDATE `addresses` set `latlongID` = $latlongID , `mapID` = $idN  WHERE `id_art` = $id1 and `ordre` = $order ";
+            $result4 = mysqli_query($link, $queryUpdate);
+            echo "<br/>".$queryUpdate;
 
 
-        }
-   // }
-   //
-   //
-        if (!$result5) {
-            echo "\n DB Error, could not query the database\n";
-            echo 'MySQL Error: ' . mysqli_error($link);
-            //exit;
         }
     }
+}
