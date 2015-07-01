62c3a08823889a0c41303598c56f464897681130
Kunwar Rattan <kunwarrattan@live.com>
2015-07-01 14:31:58 -0400

    cited

--- database/authorCode/citedAuthor.php
+++ database/authorCode/citedAuthor.php
@@ -16,58 +16,71 @@ if (!$link = mysqli_connect('localhost', 'root', '')) {
     echo 'Could not connect to mysql';
     exit;
 }
-
 if (!mysqli_select_db($link,'geolocation')) {
     echo 'Could not select database';
     exit;
 }
-
-
-$query = ' SELECT `ID_Art`,`Ordre`,`Nom`,`Pyear` FROM `authors` LIMIT 9,100' ;
+$query = ' SELECT `Cited_ID_Art`,`Publication_year` FROM `cited_papers_unique` LIMIT 1,10' ;
 
 $result = mysqli_query($link, $query);
-echo "<br/>".$query;
+echo "<br/>".$query;‏
 if($result->num_rows>0) {
     while ($row = mysqli_fetch_assoc($result)) {
         echo "<br/>-------------------------------------------";
-        $author_ID = $row['ID_Art'];
-        $ordre = $row['Ordre'];
-        $Nom = $row['Nom'];
-        $year = $row['Pyear'];
-        $idN = null;
-        echo "<br/>".$author_ID." = ".$ordre;
-        $query2 = " INSERT INTO `authortable`( `authors`, `Title`,`DOP`) VALUES (\"$Nom\",$author_ID, $year)";
-        //mysqli_query("utf8",$query);
-        echo "<br/>".$query2;
-        mysqli_set_charset($link, "utf8");
-        mysqli_query($link, "SET NAMES 'utf8'");
-        mysqli_query($link, "SET CHARACTER SET 'utf8'");
-        $result2 = mysqli_query($link, $query2);
-        $newAuthor = "";
-        $idN = mysqli_insert_id($link);
-        if($idN == null){
-            $query3 = "SELECT `id`,`authors` FROM `authortable` where `Title` = $author_ID ";
-            $result3 = mysqli_query($link, $query3);
-            echo "<br/>".$query3;
-            $flag = 0;
-            while ($row = mysqli_fetch_assoc($result3)) {
-                $idN  = $row['id'];
 
-                $authorname = $row['authors'];
+        $author_ID = $row['Cited_ID_Art'];
+        $pyear = $row['Publication_year'];
 
-                $newAuthor = $authorname.", ".$Nom;
-                echo "<br/>".$authorname."=".$Nom;
+        $query1 = " SELECT `Nom` FROM `cited_papers_authors` where `Cited_ID_Art`   = $author_ID ";
 
-                if(stristr($authorname,$Nom)){
-                    echo "<br/>".$flag;
-                    $flag = 1;
+        $result1 = mysqli_query($link, $query1);
+
+        echo "<br/>" . $query1;
+
+        if ($result1->num_rows > 0) {
+            while ($row = mysqli_fetch_assoc($result1)) {
+                $Nom = $row['authors'];
+                $idN = null;
+
+                $query2 = " INSERT INTO `cited_coauthors`( `authors`, `Title`,`DOP`) VALUES (\"$Nom\",$author_ID, $pyear)";
+                echo "<br/>" . $query2;
+
+                mysqli_set_charset($link, "utf8");
+                mysqli_query($link, "SET NAMES 'utf8'");
+                mysqli_query($link, "SET CHARACTER SET 'utf8'");
+                $result2 = mysqli_query($link, $query2);
+
+                $idN = mysqli_insert_id($link);
+
+                if ($idN == null) {
+                    $query3 = "SELECT `id`,`authors` FROM `cited_coauthors` where `Title` = $author_ID ";
+                    $result3 = mysqli_query($link, $query3);
+                    echo "<br/>".$query3;
+                    $flag = 0;
+                    while ($row = mysqli_fetch_assoc($result3)) {
+
+                        $idN  = $row['id'];
+
+                        $authorname = $row['authors'];
+
+                        $newAuthor = $authorname.", ".$Nom;
+                        echo "<br/>".$authorname."=".$Nom;
+
+                        if(stristr($authorname,$Nom)){
+                            echo "<br/>".$flag;
+                            $flag = 1;
+                        }
+                    }
+                    if($flag == 0){
+                        $queryUpdate ="\n UPDATE `cited_coauthors` set `authors` = \"$newAuthor\" WHERE `id` = $idN";
+                        $result6 = mysqli_query($link, $queryUpdate);
+                        echo "<br/>".$queryUpdate;
+                    }
                 }
             }
-            if($flag == 0){
-                $queryUpdate ="\n UPDATE `authortable` set `authors` = \"$newAuthor\" WHERE `id` = $idN";
-                $result6 = mysqli_query($link, $queryUpdate);
-                echo "<br/>".$queryUpdate;
-            }
+
         }
     }
+
+
 }
